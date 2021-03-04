54a02719b210258467e13f88205426f56741c2bb
Silent-Crafter <nikhilsolanke5@gmail.com>
2021-03-04 07:50:37 +0530

    added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo chmod +x wallpaper_slideshow.sh
+sudo chmod +x kill_ws.sh
+chmod +x wallpaper_slideshow.py
+sudo cp wallpaper_slideshow.sh /bin/wallpaper_slideshow
+sudo cp kill_ws.sh /bin/kill_ws
